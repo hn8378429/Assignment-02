be427fa8d3b67bc5a5be1e469958ec45aa0ad229
Muhammad Hasan <160401271+hn8378429@users.noreply.github.com>
2024-10-16 23:51:56 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Home = () => {
+
+  return (
+    <body>
+
+
+      <div className="main">
+        Hi,
+        <br />
+        This is <span>Muhammad Hasan</span>
+        <br /> an enthusiastic Website Developer associated with <span>NEXT-JS</span>. |
+      </div>
+
+      <div className="main">
+        <img src="https://img.freepik.com/free-photo/digital-art-style-illustration-graphic-designer_23-2151536950.jpg" alt="Dp" className="profile"/>
+
+</div>
+    </body>
+
+
+
+  )
+
+};
+
+export default Home;
